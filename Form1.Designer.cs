97baa7fd8ee81e3ecc41e17c835f07e748fdb28d
Enrique Flores <67989268+evflores001@users.noreply.github.com>
2024-02-23 14:11:30 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCreateFolder = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnCreateFolder
+            // 
+            btnCreateFolder.Location = new Point(32, 70);
+            btnCreateFolder.Margin = new Padding(2, 2, 2, 2);
+            btnCreateFolder.Name = "btnCreateFolder";
+            btnCreateFolder.Size = new Size(116, 24);
+            btnCreateFolder.TabIndex = 0;
+            btnCreateFolder.Text = "Create Folder";
+            btnCreateFolder.UseVisualStyleBackColor = true;
+            btnCreateFolder.Click += btnCreateFolder_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(151, 71);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(355, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(276, 55);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(692, 567);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(btnCreateFolder);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCreateFolder;
+        private TextBox textBox1;
+        private Label label1;
+
+    }
+}
